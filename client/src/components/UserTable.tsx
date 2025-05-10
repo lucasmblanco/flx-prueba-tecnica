71b9f5e86a7a2d674697a764196d70def87222dd
Lucas Matias Blanco <lucasmatiasblanco@outlook.com>
2025-05-10 00:52:42 -0300

    added status filter, fixed status badge text

--- client/src/components/UserTable.tsx
+++ client/src/components/UserTable.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { Table, Tag, Space, Input } from "antd";
+import { useEffect, useState, useCallback } from "react";
+import { Table, Tag, Space, Input, Select } from "antd";
 
 const { Search } = Input;
 
@@ -8,19 +8,20 @@ interface userTableProps {}
 const UserTable: React.FC<userTableProps> = () => {
   const [users, setUsers] = useState<any[]>();
   const [searchUser, setSearchUser] = useState("");
+  const [filterStatus, setFilterStatus] = useState<string>("");
 
-  async function fetchUsers() {
+  const fetchUsers = useCallback(async () => {
     const users = await fetch("http://localhost:4000/users").then((response) =>
       response.json(),
     );
     return users;
-  }
+  }, []);
 
   useEffect(() => {
     fetchUsers().then((data) => {
       setUsers(data);
     });
-  }, []);
+  }, [fetchUsers]);
 
   return (
     <>
@@ -30,6 +31,30 @@ const UserTable: React.FC<userTableProps> = () => {
           placeholder="Busqueda por nombre o apellido"
           allowClear
           onSearch={(value) => setSearchUser(value.toLowerCase())}
+        />
+      </Space.Compact>
+      <Space.Compact>
+        <Select
+          placeholder="Filtrar por estado"
+          optionFilterProp="label"
+          onChange={(value: string | undefined) => {
+            if (!value) {
+              setFilterStatus("");
+            } else {
+              setFilterStatus(value);
+            }
+          }}
+          allowClear
+          options={[
+            {
+              value: "inactive",
+              label: "Inactivo",
+            },
+            {
+              value: "active",
+              label: "Activo",
+            },
+          ]}
         />
       </Space.Compact>
       {users?.length ? (
@@ -57,27 +82,33 @@ const UserTable: React.FC<userTableProps> = () => {
                 if (value === "") {
                   return true;
                 }
-                const fullname =
-                  record.name.toLowerCase() +
-                  " " +
-                  record.lastname.toLowerCase();
+                const fullname = `${record.name.toLowerCase()} ${record.lastname.toLowerCase()}`;
                 return fullname.includes(value);
               },
             },
             {
               title: "Estado",
               dataIndex: "status",
+              filteredValue: [filterStatus],
+              onFilter: (value, record) => {
+                if (value === "") {
+                  return true;
+                }
+                return record.status === value;
+              },
               render: (_, { status }) => {
                 let color;
+                let statusTranslated;
                 if (status === "active") {
                   color = "green";
+                  statusTranslated = "ACTIVO";
                 } else {
                   color = "volcano";
+                  statusTranslated = "INACTIVO";
                 }
-
                 return (
                   <Tag color={color} key={status}>
-                    {status.toUpperCase()}
+                    {statusTranslated}
                   </Tag>
                 );
               },
